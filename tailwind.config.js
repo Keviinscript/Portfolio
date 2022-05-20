00b1dc433bfab7442714e4664903696bef810fdf
ikechukwwu <ikechukwukevin725@gmail.com>
2022-05-20 06:37:31 -0400

    initial  commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    fontFamily: {
+      'culpa':['Montserrat', 'sans-serif'],
+      'incon':['Barlow Semi Condensed', 'sans-serif']
+    },
+    extend: {
+      spacing:{
+        '128': '50rem',
+
+      }
+    },
+  },
+  plugins: [],
+}
